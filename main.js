8d3e162e18d2c32e041c25a65fb42952bf60c68e
Edvinas1982 <79442331+Edvinas1982@users.noreply.github.com>
2023-02-03 15:24:58 +0200

    add_satrting_data_to_table

--- main.js
+++ main.js
@@ -2,6 +2,16 @@
 var addData = document.getElementById('addData');
 var modal1 = document.getElementById('modal1');
 var cancel = document.getElementById('cancel');
+var confirm = document.getElementById('confirm');
+var someData = [
+    [122, 'string', 456, 44, 11 ],
+    [221, 'another string', 555, 55, 123]
+];
+// add starting data to table
+
+
+
+tbody.appendChild(tr);
 // add button calling modal
 addData.addEventListener('click', function(){
     modal1.classList.add('modal--active');
@@ -9,4 +19,8 @@ addData.addEventListener('click', function(){
 // cancel button close modal 
 cancel.addEventListener('click', function(){
     modal1.classList.remove('modal--active');
+});
+// confirm button and add data to table
+confirm.addEventListener('click', function(){
+
 });
