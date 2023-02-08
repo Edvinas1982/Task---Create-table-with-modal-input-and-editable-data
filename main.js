8d20681c20ca3d4126ebb354a12db5aa05da7242
Edvinas1982 <79442331+Edvinas1982@users.noreply.github.com>
2023-02-08 13:02:52 +0200

    add_function

--- main.js
+++ main.js
@@ -1,26 +1,45 @@
 // Variables
-var addData = document.getElementById('addData');
-var modal1 = document.getElementById('modal1');
-var cancel = document.getElementById('cancel');
-var confirm = document.getElementById('confirm');
+var addData = document.getElementById("addData");
+var modal1 = document.getElementById("modal1");
+var cancel = document.getElementById("cancel");
+var confirm = document.getElementById("confirm");
+var tbody = document.querySelector("tbody");
+var data1 = document.getElementById("data1");
+var data2 = document.getElementById("data2");
 var someData = [
-    [122, 'string', 456, 44, 11 ],
-    [221, 'another string', 555, 55, 123]
+  [122, "string", 123, 124, 125],
+  [221, "another string", 126, 27, 8],
 ];
-// add starting data to table
+// function witch add starting data to table
+function printTable() {
+    tbody.innerHTML = '';
+  for (var i = 0; i < someData.length; i++) {
+    var tr = document.createElement("tr");
+    for (var j = 0; j < someData[i].length; j++) {
+      var td = document.createElement("td");
+      td.innerText = someData[i][j];
+      tr.appendChild(td);
+    }
+    tbody.appendChild(tr);
+  }
+  tbody.appendChild(tr);
+}
+printTable();
 
-
-
-tbody.appendChild(tr);
 // add button calling modal
-addData.addEventListener('click', function(){
-    modal1.classList.add('modal--active');
+addData.addEventListener("click", function () {
+  modal1.classList.add("modal--active");
 });
-// cancel button close modal 
-cancel.addEventListener('click', function(){
-    modal1.classList.remove('modal--active');
+// cancel button close modal
+cancel.addEventListener("click", function () {
+  modal1.classList.remove("modal--active");
 });
-// confirm button and add data to table
-confirm.addEventListener('click', function(){
-
+// confirm button and add new data to table
+confirm.addEventListener("click", function () {
+  var newData = [data1.value, data2.value];
+  someData.unshift(newData);
+  printTable();
+  modal1.classList.remove("modal--active");
+  data1.value ='';
+  data2.value ='';
 });
